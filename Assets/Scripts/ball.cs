1d140fd5ec1df6aebe0296b16d81359dd88ecff2
G-VosFox <144125620+G-VosFox@users.noreply.github.com>
2023-09-29 12:50:36 +0200

    the ball moves

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float xPosition = 1f;
+    public float yPosition = 1f;
+    public float xPace = 1f;
+    public float yPace = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xPosition += xPace * Time.deltaTime;
+        yPosition += yPace * Time.deltaTime;
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+}
